a2a9f797e5a4fb2fbdf9efe327447307ff8dd333
Kai Camargo <kaicamargo@protonmail.com>
2025-12-17 12:34:47 -0800

    Create AutoMode.java
    The start of an autonomous era

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "Auto Mode", group = "Autonomous")
+public class AutoMode extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
